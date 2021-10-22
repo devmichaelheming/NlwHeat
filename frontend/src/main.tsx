0baefcae2c6f96ed116fb041ca1cac5b5965e291
devmichaelheming <devmichael.heming@gmail.com>
2021-10-22 16:06:46 -0400

    Finalizado frontend da aplicação em react js

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import './styles/global.css';
+import { AuthProvider } from "./context/auth";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
